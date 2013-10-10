f64650e07fe36baef6cc199787f4e4559692018e
Wallace Sidhrée <i@dreamyguy.com>
2013-10-10 16:19:53 +0200

    Introduce all the fundamental goodness

---
+++ src/js/js_bottom.js
@@ -0,0 +1,3 @@
+// go - v0.0.0 - 2013-10-10 16:10:27
+
+function hw(){return"Hello World"}var myAppModule=angular.module("myApp",[]);myAppModule.controller("myController",function($scope){$scope.showOnLoad=!0});
